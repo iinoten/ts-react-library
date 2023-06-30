356d44f22f6deafed9d763d123cb61ac2dc5b266
iinoten <nteten0313@icloud.com>
2023-07-01 04:03:52 +0900

    Add Headingコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Heading } from "./libs/Heading"
 import { Text } from "./libs/Text"
 
 export const App = () => {
     return (
-      <Text text={"truse"} />
+        <>
+            <Text text={"truse"} />
+            <Heading tag='h1'>
+                見出し
+            </Heading>
+
+            <Heading tag='h1'>
+                <span>hello world!</span>
+            </Heading>
+        </>
     )
   }
